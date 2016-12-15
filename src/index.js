0b3acc652292066a449f2ce3c19734e5c241808d
Michael Gord <michael.l.gord@gmail.com>
2016-12-15 00:18:08 -0500

    frontend works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-// Create a new component. This component should produce some HTML
-const App = function() {
-	return <div>This works!</div>;
-}
-// Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
